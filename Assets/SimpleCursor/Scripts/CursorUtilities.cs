5539659244d1107a1c98afb92f47951c9ab90336
jnphgs <mails@jnphgs.net>
2019-06-02 02:36:39 +0900

    Add 13 cursor types

--- Assets/SimpleCursor/Scripts/CursorUtilities.cs
+++ Assets/SimpleCursor/Scripts/CursorUtilities.cs
@@ -9,10 +9,23 @@ namespace SimpleCursor
         private readonly static Dictionary<CursorType, Texture2D> resources = new Dictionary<CursorType, Texture2D>()
         {
             [CursorType.Default] = Load("SimpleCursor/Textures/default"),
+            [CursorType.Move] = Load("SimpleCursor/Textures/move"),
             [CursorType.Resize_N] = Load("SimpleCursor/Textures/n-resize"),
             [CursorType.Resize_E] = Load("SimpleCursor/Textures/e-resize"),
             [CursorType.Resize_W] = Load("SimpleCursor/Textures/w-resize"),
             [CursorType.Resize_S] = Load("SimpleCursor/Textures/s-resize"),
+            [CursorType.Resize_NE] = Load("SimpleCursor/Textures/ne-resize"),
+            [CursorType.Resize_NW] = Load("SimpleCursor/Textures/nw-resize"),
+            [CursorType.Resize_SE] = Load("SimpleCursor/Textures/se-resize"),
+            [CursorType.Resize_SW] = Load("SimpleCursor/Textures/sw-resize"),
+            [CursorType.Rotate_N] = Load("SimpleCursor/Textures/n-rotate"),
+            [CursorType.Rotate_E] = Load("SimpleCursor/Textures/e-rotate"),
+            [CursorType.Rotate_W] = Load("SimpleCursor/Textures/w-rotate"),
+            [CursorType.Rotate_S] = Load("SimpleCursor/Textures/s-rotate"),
+            [CursorType.Rotate_NE] = Load("SimpleCursor/Textures/ne-rotate"),
+            [CursorType.Rotate_NW] = Load("SimpleCursor/Textures/nw-rotate"),
+            [CursorType.Rotate_SE] = Load("SimpleCursor/Textures/se-rotate"),
+            [CursorType.Rotate_SW] = Load("SimpleCursor/Textures/sw-rotate"),
         };
 
         private static Texture2D Load(string filename)
@@ -39,9 +52,22 @@ namespace SimpleCursor
     public enum CursorType
     {
         Default,
+        Move,
         Resize_N,
         Resize_E,
         Resize_W,
         Resize_S,
+        Resize_NE,
+        Resize_NW,
+        Resize_SE,
+        Resize_SW,
+        Rotate_N,
+        Rotate_E,
+        Rotate_W,
+        Rotate_S,
+        Rotate_NE,
+        Rotate_NW,
+        Rotate_SE,
+        Rotate_SW,
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 # UnitySimpleCursor
+
+![Cover](Cover.gif)
+
+## Introduction
+Change cursor for selection, panning, rotation, resizing.
+
+
+## Usage
+
+```cs
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HoverElement : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+  public void OnPointerEnter(PointerEventData eventData)
+  {
+      SimpleCursor.CursorUtilities.ChangeCursor(CursorType.Resize_N);
+  }
+
+  public void OnPointerExit(PointerEventData eventData)
+  {
+      SimpleCursor.CursorUtilities.ChangeCursor(CursorType.Default);
+  }
+}
+```
+
+1. Call `SimpleCursor.CursorUtilities.ChangeCursor(CursorType type)` on hover.
+2. Call `SimpleCursor.CursorUtilities.ChangeCursor(CursorType.Default)` on unhober.
+
+Following CursorTypes are implemented.
+- Default
+- Move
+- Resize_N
+- Resize_E
+- Resize_W
+- Resize_S
+- Resize_NE
+- Resize_NW
+- Resize_SE
+- Resize_SW
+- Rotate_N
+- Rotate_E
+- Rotate_W
+- Rotate_S
+- Rotate_NE
+- Rotate_NW
+- Rotate_SE
+- Rotate_SW
+
+
+## Environment
+- Unity 2018.4.1f1
