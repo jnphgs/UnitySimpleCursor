7daed272b0dc46b696c08ee174a0b037107621fb
jnphgs <mails@jnphgs.net>
2019-06-02 01:59:16 +0900

    Add Project files

---
+++ Assets/SimpleCursor/Scripts/CursorUtilities.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SimpleCursor
+{
+    public static class CursorUtilities
+    {
+        private readonly static Dictionary<CursorType, Texture2D> resources = new Dictionary<CursorType, Texture2D>()
+        {
+            [CursorType.Default] = Load("SimpleCursor/Textures/default"),
+            [CursorType.Resize_N] = Load("SimpleCursor/Textures/n-resize"),
+            [CursorType.Resize_E] = Load("SimpleCursor/Textures/e-resize"),
+            [CursorType.Resize_W] = Load("SimpleCursor/Textures/w-resize"),
+            [CursorType.Resize_S] = Load("SimpleCursor/Textures/s-resize"),
+        };
+
+        private static Texture2D Load(string filename)
+        {
+            var tex = Resources.Load<Texture2D>(filename);
+            return tex;
+        }
+
+        public static void ChangeCursor(CursorType cursor)
+        {
+            if(cursor == CursorType.Default)
+            {
+                Cursor.SetCursor(null, Vector2.zero, CursorMode.ForceSoftware);
+            }
+            else
+            {
+                var tex = resources[cursor];
+
+                Cursor.SetCursor(tex, new Vector2(0.5f * tex.width, 0.5f * tex.height), CursorMode.Auto);
+            }
+        }
+    }
+
+    public enum CursorType
+    {
+        Default,
+        Resize_N,
+        Resize_E,
+        Resize_W,
+        Resize_S,
+    }
+}
---
+++ Assets/SimpleCursor/Scripts/HoverElement.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace SimpleCursor{
+
+    public class HoverElement : MonoBehaviour
+        , IPointerClickHandler
+         , IDragHandler
+         , IPointerEnterHandler
+         , IPointerExitHandler
+    {
+        public Text text;
+        public CursorType Type;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void OnValidate() // call in editor
+        {
+            if (text != null)
+            {
+                text.text = Type.ToString();
+            }
+            gameObject.name = Type.ToString();
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            CursorUtilities.ChangeCursor(Type);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            CursorUtilities.ChangeCursor(CursorType.Default);
+        }
+    }
+
+
+}
